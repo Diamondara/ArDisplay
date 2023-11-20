6c280ad72a99bbcc543c558e8fdd257cb0546291
Diamondara <diamondara@web.de>
2023-11-20 15:57:01 +0100

    df

--- Assets/maximizeObject.cs
+++ Assets/maximizeObject.cs
@@ -8,16 +8,19 @@ public class maximizeObject : MonoBehaviour
 {
     GameObject objectBigger;
     TextMeshProUGUI testtext;
-    public float step = 0.5f;
+    TextMeshProUGUI massstab;
+    public float step = 0.001f;
      
     public void MaximizeObject()
     {
         testtext = GameObject.Find("testtext").GetComponent<TextMeshProUGUI>();
+        massstab = GameObject.Find("massstab").GetComponent<TextMeshProUGUI>();
         objectBigger = GameObject.Find("modelchanger(Clone)");
-        if (objectBigger.transform.localScale.x + step < 4f)
+        if (objectBigger.transform.localScale.x + step < 0.01f)
         {
             testtext.text = "Objekt grˆﬂer skaliert.";
            objectBigger.transform.localScale += new Vector3(step,step,step);
+            massstab.text = "Maﬂstab: 1:" + Mathf.Round(1 / objectBigger.transform.localScale.x * 1f) / 1f;
         }
         else { 
             testtext.text = "Das Objekt kann nicht grˆﬂer skaliert werden.";
@@ -27,11 +30,13 @@ public class maximizeObject : MonoBehaviour
     public void MinimizeObject()
     {
         testtext = GameObject.Find("testtext").GetComponent<TextMeshProUGUI>();
+        massstab = GameObject.Find("massstab").GetComponent<TextMeshProUGUI>();
         objectBigger = GameObject.Find("modelchanger(Clone)");
         if (objectBigger.transform.localScale.x - step > 0)
         {
             testtext.text = "Objekt kleiner skaliert.";
            objectBigger.transform.localScale -= new Vector3(step, step, step);
+            massstab.text = "Maﬂstab: 1:" + Mathf.Round(1/objectBigger.transform.localScale.x*1f)/1f;
         }
         else {
             testtext.text = "Das Objekt kann nicht kleiner skaliert werden.";
--- Assets/rotateObject.cs
+++ Assets/rotateObject.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class rotateObject : MonoBehaviour
 {
     GameObject objectRotate;
-
+ 
     public void RotateObject() {
         objectRotate = GameObject.Find("modelchanger(Clone)");
         objectRotate.transform.Rotate(Vector3.up, 45);
